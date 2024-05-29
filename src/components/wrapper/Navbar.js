1edb228b4ef77287fe46e38818f2804f84212046
Vivien Laquay <vivien.laquay@gmail.com>
2024-05-29 19:25:53 +0200

    certif

--- src/components/wrapper/Navbar.js
+++ src/components/wrapper/Navbar.js
@@ -80,7 +80,7 @@ const Navbar = () => {
                   </NavLink>
                   <NavLink
                     onClick={logout}
-                    to="/connexion"
+                    to="/"
                     className="svg-container"
                   >
                     <img src={Logout} alt="deconnexion" />
